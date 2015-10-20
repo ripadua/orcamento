2cb08afc05d5608b7842d14039cc1531f99eec05
Ricardo Pádua Crispim <ripadua7@gmail.com>
2015-10-20 16:23:42 +0000

    Inclusao de gif de loading para chamadas ajax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,3 +23,14 @@
 //= require bootstrap
 //= require bootstrap-datepicker
 //= require_tree .
+
+$(function() {
+  $('#loading-indicator').hide();  // hide it initially.
+  $(document)  
+    .ajaxStart(function() {
+      $('#loading-indicator').show(); // show on any Ajax event.
+    })
+    .ajaxStop(function() {
+      $('#loading-indicator').hide(); // hide it when it is done.
+  });
+});
